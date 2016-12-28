6cac1c4ba8924bb1e7dbe14d296a1c0231601cfc
Saurabh Ajmera <sajmera@live.com>
2016-12-27 18:22:27 -0800

    completing the rest service for todo

--- routes/todos.js
+++ routes/todos.js
@@ -6,12 +6,101 @@
  */
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://sajmera:1985narimanpoint@ds145118.mlab.com:45118/meantodos-sajmera',['todos']);
 
-router.get('/',function(req,res,next){
-    res.send('TODO\'s API');
+
+router.get('/todos',function(req,res,next){
+    db.todos.find(function (err, todos) {
+        if(err){
+            res.send(err);
+        }else{
+            res.json(todos);
+        }
+    });
+});
+
+
+router.get('/todos/:id',function (req,res,next) {
+    db.todos.findOne({
+        _id:mongojs.ObjectId(req.params.id)
+
+    },function (err,todos) {
+        if(err){
+            res.send(err);
+        }else{
+            res.json(todos);
+        }
+
+    })
 
 });
 
+//Save todo
+
+router.post('/todo',function(req,res,next){
+    var todo = req.body;
+    if(!todo.text || !(todo.isCompleted + '')) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    }else{
+        db.todos.save(todo,function(err,result){
+            if(err){
+                res.send(err);
+            }else{
+                res.json(result);
+            }
+        });
+
+    }
+
+
+});
+
+//Update Todo
+router.put('/todo/:id',function (req,res,next) {
+    var todo = req.body;
+    var updateObject = {};
+
+    if(todo.isCompleted){
+        updateObject.isCompleted = todo.isCompleted;
+    }
+
+    if(todo.text){
+        updateObject.text = todo.text;
+    }
+
+    if(!updateObject){
+        res.status(400);
+        res.json({
+            "error":"Invalid Data!"
+        });
+    }else{
+        db.todos.update({
+            _id:mongojs.ObjectID(req.params.id)
+        },updateObject,function (err,result) {
+            if(err){
+                res.send(err);
+            }else{
+                res.json(result);
+            }
+        });
+    }
+
+});
+
+router.delete('/todo/:id',function (req,res) {
+    db.todos.remove({
+        _id:mongojs.ObjectID(req.params.id)
+    },function (err,result) {
+        if(err){
+            res.send(err);
+        }else{
+            res.json(result);
+        }
+    });
+});
+
 module.exports = router;
-
-
