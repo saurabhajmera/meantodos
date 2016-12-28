b8ba91f1070949569b9c8b94e565e1fef76c8fff
Saurabh Ajmera <sajmera@live.com>
2016-12-28 10:31:20 -0800

    adding the client application

---
+++ client/app/todos/todos.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by sajmera on 12/27/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId:module.id,
+  selector: 'todos',
+  templateUrl: './todos.component.html',
+})
+export class TodosComponent  {
+
+
+}
